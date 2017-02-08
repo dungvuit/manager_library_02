16a7b99332c0d0948e97ebad974f269b14bf4bb3
Nxthien <nxthien58@gmail.com>
2017-02-08 22:35:06 +0700

    sign in, sign up

--- lib_manager/db/schema.rb
+++ lib_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207044937) do
+ActiveRecord::Schema.define(version: 20170208144343) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20170207044937) do
     t.boolean  "is_admin",        default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "remember_digest"
   end
 
 end
