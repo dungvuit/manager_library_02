df4b0261871d6684ee2a01ba6f63688c9a4866df
Nxthien <nxthien58@gmail.com>
2017-02-10 10:07:30 +0700

    Searching

--- lib_manager/app/assets/javascripts/books.js
+++ lib_manager/app/assets/javascripts/books.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+document.addEventListener("turbolinks:load", function() {
   $('.list-categories a').on('click', 'li', function(){
     $('.list-categories a li').removeClass('active');
     $(this).addClass('active')
--- lib_manager/app/controllers/books_controller.rb
+++ lib_manager/app/controllers/books_controller.rb
@@ -8,6 +8,9 @@ class BooksController < ApplicationController
     if params[:category_id].present?
       find_category
       @books = @category.books.pagination params[:page]
+    elsif params[:search].present?
+      search_book
+      @books = @books.pagination params[:page]
     else
       @books = Book.pagination params[:page]
     end
@@ -26,9 +29,17 @@ class BooksController < ApplicationController
     @book = Book.find_by id: params[:id]
     check_object @book
   end
-  
+
   def find_category
     @category = Category.find_by id: params[:category_id]
     check_object @category
+  end
+
+  def search_book
+    @books = Book.all
+    if params[:search].present?
+      @books = @books.search_book params[:search][:book_name], params[:search][:author_name],
+        params[:search][:category_name], params[:search][:publisher_name]
+    end
   end
 end
